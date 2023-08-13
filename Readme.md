f84374f1be76ec4215cf85763bf498b2edef9a9b
AlexCSDev <942091+AlexCSDev@users.noreply.github.com>
2023-08-13 04:29:00 +0300

    Pixiv ugoira support, changed endpoint address

--- Readme.md
+++ Readme.md
@@ -1,12 +1,20 @@
-# Twitter video API for Hydrus Network
+# Video Proxy API for Hydrus Network
 Based on https://github.com/erickythierry/BaixarVideoTwitterAPI
 
+##Supported sites:
+* Twitter
+* Pixiv (ugoira)
+
 ## Installation:
+- Make sure ffmpeg is installed
 - `pip -r requirements.txt`
 - Dump your twitter cookies in netscape format into `cookies.txt`
+- Configure your pixiv refresh token in `gallery-dl.conf` if you are going to be downloading pixiv ugoira
 - Run the api using `python api.py`
-- Open `hydrus_content_parser_video.json` and change `api_address` to address of the api
-- Import `hydrus_content_parser_video.json` into your twitter parser via content parsers tab
+- Open `hydrus_twitter_content_parser_video.json`, `hydrus_pixiv_content_parser_ugoira.json` and change `api_address` to address of the api
+- Import `hydrus_content_parser_video.json`, `hydrus_pixiv_content_parser_ugoira.json` into your twitter and pixiv parsers via content parsers tab
+- Delete `ugoira veto` from pixiv content parsers
+- Depending on the machine api is running on and its internet connection speed you might want to increase `network timeout (seconds)` under `file -> options -> connection` to 30 seconds or more. Especially if you are downloading ugoira.
 
 ## Standalone API usage
-Api can also be used in other software by accessing it via the following url: `http://api_address:5000/get_tweet_video?url=https://twitter.com/user/status/1234567890`
+Api can also be used in other software by accessing it via the following url: `http://api_address:5000/get_video?url=https://twitter.com/user/status/1234567890`
--- api.py
+++ api.py
@@ -3,64 +3,99 @@ import os
 import time
 import yt_dlp
 import uuid
-from dotenv import load_dotenv
-
-load_dotenv()
+import subprocess
+import platform
 
 app = Flask(__name__)
 app.config['DOWNLOAD_FOLDER'] = 'downloads'
 
 def delete_old_files():
-    folder_path = 'downloads'
-    file_extension = '.mp4'
     time_threshold = 120  # 2 minutes in seconds
 
     current_time = time.time()
 
-    for filename in os.listdir(folder_path):
-        if filename.endswith(file_extension):
-            file_path = os.path.join(folder_path, filename)
+    for filename in os.listdir(app.config['DOWNLOAD_FOLDER']):
+        if filename.endswith('.mp4') or filename.endswith('.webm'):
+            file_path = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
             try:
                 creation_time = os.path.getctime(file_path)
                 if (current_time - creation_time) > time_threshold:
                     os.remove(file_path)
-                    print(f"Removed '{filename}' due to reaching time threshold.")
+                    print(f"File '{filename}' has been deleted.")
             except Exception as e:
-                print(f"Error while removing '{filename}': {str(e)}")
+                print(f"Error removing file '{filename}': {str(e)}")
 
+def download_using_gallerydl(url, videoRandomID):
+    cmd_line = ['gallery-dl']
+    cmd_line += ['--verbose']
+    cmd_line += ['--cookies', os.getcwd() + '/cookies.txt']
+    cmd_line += ['--ignore-config']
+    cmd_line += ["-c", os.getcwd() + "/gallery-dl.conf"]
+    cmd_line += ['--directory', os.getcwd() + '/' + app.config['DOWNLOAD_FOLDER']]
+    cmd_line += ['--filename', videoRandomID + '.webm']
+    cmd_line += ['--ugoira-conv-lossless']
+    cmd_line += ['-o', 'signals-ignore=SIGTTOU,SIGTTIN']
+    cmd_line += [url]
 
-def download_video(url, videoRandomID):
+    if platform.system() == 'Windows':
+        popen = subprocess.Popen(cmd_line, text = True, creationflags = subprocess.CREATE_NEW_PROCESS_GROUP)
+    else:
+        popen = subprocess.Popen(cmd_line, text = True, preexec_fn = os.setpgrp)
 
+    popen.wait()
+    
+    return check_gallerydl_return_code(popen.returncode)
+    
+def check_gallerydl_return_code(code: int):
+    errors = []
+    if code & 1: errors.append("unspecified error")
+    if code & 2: errors.append("cmdline arguments")
+    if code & 4: errors.append("http error")
+    if code & 8: errors.append("not found / 404")
+    if code & 16: errors.append("auth / login")
+    if code & 32: errors.append("format or filter")
+    if code & 64: errors.append("no extractor")
+    if code & 128: errors.append("os error")
+    return ", ".join(errors)
+
+def download_using_ytdlp(url, videoRandomID):
     ydl_opts = {
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'merge_output_format': 'mp4',
         'cookiefile': 'cookies.txt',
-        'outtmpl': os.path.join(app.config['DOWNLOAD_FOLDER'], videoRandomID+'.mp4')
+        'outtmpl': os.path.join(app.config['DOWNLOAD_FOLDER'], videoRandomID + '.mp4')
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-
 @app.route('/', methods=['GET'])
 def index():
-    return "running ✅"
+    return 'running ✅'
 
-@app.route('/get_tweet_video', methods=['GET'])
+@app.route('/get_video', methods=['GET'])
 def get_tweet_video():
     delete_old_files()
     video_url = request.args.get('url')
-    videoRandomID = str(uuid.uuid4()).rsplit('-', 1)[-1]
+    videoRandomID = str(uuid.uuid4()).rsplit('-', 1)[-1] #todo: generate from url so already downloaded files are reused
 
     if video_url:
-        if 'twitter.com' not in video_url:
-            return jsonify({'error': 'twitter.com url is expected'})
+        if 'twitter.com' not in video_url and 'https://www.pixiv.net/en/artworks/' not in video_url:
+            return jsonify({'error': 'twitter.com or pixiv.net artwork url is expected'})
         try:
-            download_video(video_url, videoRandomID)
-            return send_from_directory(app.config['DOWNLOAD_FOLDER'], f"{videoRandomID}.mp4")
-        except:
-            return jsonify({'error': 'error while downloading video'}), 400
-
+            ext = 'mp4'
+            if 'https://www.pixiv.net/en/artworks/' in video_url:
+                gdl_result = download_using_gallerydl(video_url, videoRandomID)
+                if gdl_result:
+                    raise Exception(f"Gallery-dl failed: {gdl_result}")
+                ext = 'webm'
+            else:
+                download_using_ytdlp(video_url, videoRandomID)
+                
+            return send_from_directory(app.config['DOWNLOAD_FOLDER'], f'{videoRandomID}.{ext}')
+        except Exception as err:
+            print(err)
+            return jsonify({'error': 'error while downloading video', 'error_details': str(err)}), 400
     else:
         return jsonify({'error': 'Invalid url'}), 500
 
